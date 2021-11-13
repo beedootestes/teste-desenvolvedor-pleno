386b4794157b635793a12084d95aea51f4263873
Luís Caputo <luiscaputo15@gmail.com>
2021-11-13 23:28:05 +0100

    :zap: [fix] tests failed

--- __tests__/createQuestion.spec.ts
+++ __tests__/createQuestion.spec.ts
@@ -1,13 +1,11 @@
-import QuestionService from "../src/services/createQuestion.service";
-import QuestionsRepository from "../src/repositories/question.repository";
-import { getCustomRepository } from "typeorm";
+import CreateQuestionService from "../src/services/createQuestion.service";
 
 describe("Create Question", () => {
   it("New Question", async () => {
-    const questioService = new QuestionService();
+    const questioService = new CreateQuestionService();
     const save = await questioService.execute({
       question: "O teste Passou?",
-      description: "teste",
+      description: "teste teste teste teste",
     });
     expect(save).toHaveProperty("id");
     expect(save.question).toBe("O teste Passou?");
