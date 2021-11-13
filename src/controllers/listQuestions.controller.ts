9fed2c1f879690271120f8819651c7749837d072
Luís Caputo <luiscaputo15@gmail.com>
2021-11-13 22:46:06 +0100

    :zap: Endpoint to ListQuestions done

---
+++ src/controllers/listQuestions.controller.ts
@@ -0,0 +1,32 @@
+import { AppResponse } from "../@types";
+import { Questions } from "../models/Questions";
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import QuestionsRepository from "../repositories/question.repository";
+
+export default class ListQuestionsController {
+  async handle(request: Request, response: Response<AppResponse<Questions[]>>) {
+    try {
+      const questionRepository = getCustomRepository(QuestionsRepository),
+        getAllQuestions = await questionRepository
+          .createQueryBuilder()
+          .select("questions")
+          .from(Questions, "questions")
+          .getMany();
+      if (getAllQuestions.length >= 0) {
+        return response.status(200).json({
+          success: true,
+          message: "All Questions",
+          data: getAllQuestions,
+        });
+      }
+      return response
+        .status(400)
+        .json({ success: false, message: "No Question Registred!" });
+    } catch (err) {
+      return response
+        .status(500)
+        .json({ success: false, message: err.message });
+    }
+  }
+}
--- src/routes/question.routes.ts
+++ src/routes/question.routes.ts
@@ -1,15 +1,17 @@
 import CreateQuestionController from "../controllers/createQuestion.controller";
 import { createQuestion } from "../middlewares/createQuestion";
 import { Router } from "express";
+import ListQuestionsController from "../controllers/listQuestions.controller";
 
 const routes = Router();
 
 const createQuestionController = new CreateQuestionController();
+const listQuestionsController = new ListQuestionsController();
 
 routes.post(
-  "/question/create",
+  "/questions/create",
   createQuestion,
   createQuestionController.handle
 );
-
+routes.get("/questions/list", listQuestionsController.handle);
 export default routes;
