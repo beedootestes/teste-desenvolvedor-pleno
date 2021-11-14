3f1387ab51fc5db0ac5b2a81f2d65a5b358f4374
Luís Caputo <luiscaputo15@gmail.com>
2021-11-14 20:44:32 +0100

    :zap: Docker In Orbit

--- src/routes/question.routes.ts
+++ src/routes/question.routes.ts
@@ -1,16 +1,16 @@
-import CreateQuestionController from "../controllers/questionsControllers/createQuestion.controller";
-import { createQuestion } from "../middlewares/createQuestion";
 import { Router } from "express";
+import { createQuestion } from "../middlewares/createQuestion";
 import ListQuestionsController from "../controllers/questionsControllers/listQuestions.controller";
 import AlterQuestionController from "../controllers/questionsControllers/alterQuestion.controller";
 import DelectQuestionController from "../controllers/questionsControllers/delectQuestion.controller";
+import CreateQuestionController from "../controllers/questionsControllers/createQuestion.controller";
 import ListQuestionsDelectedController from "../controllers/questionsControllers/questionsDelected.controller";
 
 const routes = Router();
 
-const createQuestionController = new CreateQuestionController();
 const listQuestionsController = new ListQuestionsController();
 const alterQuestionController = new AlterQuestionController();
+const createQuestionController = new CreateQuestionController();
 const delectQuestionController = new DelectQuestionController();
 const listQuestionDelectedsController = new ListQuestionsDelectedController();
 
@@ -19,8 +19,9 @@ routes.post(
   createQuestion,
   createQuestionController.handle
 );
-routes.put("/questions/alter", alterQuestionController.handle);
 routes.get("/questions/list", listQuestionsController.handle);
+routes.put("/questions/alter", alterQuestionController.handle);
 routes.get("/questions/delecteds", listQuestionDelectedsController.handle);
 routes.put("/questions/delect/:idQuestion", delectQuestionController.handle);
+
 export default routes;
