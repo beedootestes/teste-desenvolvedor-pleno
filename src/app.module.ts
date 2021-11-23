193de7abeb8432157857eb97b49738f810b20149
Cristiano <cristianovelkan@gmail.com>
2021-11-23 13:49:49 -0300

    feat: type orm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'locahost',
+      port: Number(process.env.DB_PORT) || 5432,
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASS || 'docker',
+      database: process.env.DB_NAME || 'postgres',
+      entities: [__dirname + '/**/*.entity.js'],
+      autoLoadEntities: false,
+      synchronize: false,
+    }),
+  ],
 })
-export class AppModule {}
+export class AppModule { }
