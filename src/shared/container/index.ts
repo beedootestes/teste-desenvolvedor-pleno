28f7660b5f1e968eb9685ae37b4e14eb619e6e6e
KevenJunco <49709096+KevenJunco@users.noreply.github.com>
2021-11-30 13:50:25 -0300

    Update index.ts

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,9 +1,17 @@
 import {container} from 'tsyringe';
 import { QuestionsRepository } from '@modules/questions/infra/typeorm/repositories/QuestionsRepository';
 import { IQuestionsRepository } from '@modules/questions/repositories/IQuestionsRepository';
+import { IAnswersRepository } from '@modules/questions/repositories/IAnswersRepository';
+import { AnswersRepository } from '@modules/questions/infra/typeorm/repositories/AnswersRespostory';
+
 
 
 container.registerSingleton<IQuestionsRepository>(
     "QuestionsRepository",
     QuestionsRepository
+);
+
+container.registerSingleton<IAnswersRepository>(
+    "AnswersRepository",
+    AnswersRepository
 );
