efe19b9f6fb54f35fc46fc86c991013a7232d946
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-01 18:39:52 -0300

    chore: add jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/*-protocols.ts', '!**/protocols/**', '!**/test/**'],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+}
