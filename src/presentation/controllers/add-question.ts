585e5ed7cbc802a6740ca5531823c1334a723471
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-02 09:05:35 -0300

    refactor: create helper method

--- src/presentation/controllers/add-question.ts
+++ src/presentation/controllers/add-question.ts
@@ -1,11 +1,9 @@
 import { MissingParamError } from '../errors/missing-param-error'
+import { badRequest } from '../helpers/http-helpers'
 import { httpRequest, httpResponse } from '../protocols/http'
 
 export class AddQuestionController {
   handle (httpRequest: httpRequest): httpResponse {
-    return {
-      statusCode: 400,
-      body: new MissingParamError('question')
-    }
+    return badRequest(new MissingParamError('question'))
   }
 }
