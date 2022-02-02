8ff51ec3540cb3c0722cc9f2e4283730768f62c9
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-02 10:35:25 -0300

    test: ensure AddQuestionController calls addQuestion with correct values

--- src/presentation/controllers/add-question.spec.ts
+++ src/presentation/controllers/add-question.spec.ts
@@ -1,17 +1,54 @@
+import { AddQuestion, AddQuestionModel } from '../../domain/usecases/add-question'
 import { MissingParamError } from '../errors/missing-param-error'
 import { AddQuestionController } from './add-question'
 
 describe('AddQuestion Controller', () => {
-  const makeSut = (): AddQuestionController => {
-    return new AddQuestionController()
+  interface Sut {
+    sut: AddQuestionController
+    addQuestionStub: AddQuestion
   }
+
+  const makeSut = (): Sut => {
+    const addQuestionStub = makeAddQuestion()
+    const sut = new AddQuestionController(addQuestionStub)
+    return {
+      sut,
+      addQuestionStub
+    }
+  }
+
+  const makeAddQuestion = (): AddQuestion => {
+    class AddQuestionStub implements AddQuestion {
+      add (question: AddQuestionModel): any {
+        const fakeQuestion = {
+          id: 'valid_id',
+          question: 'valid_question'
+        }
+        return fakeQuestion
+      }
+    }
+    return new AddQuestionStub()
+  }
+
   test('Should return 400 if no question is provided', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {}
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('question'))
+  })
+
+  test('Should call addQuestion with correct values', () => {
+    const { sut, addQuestionStub } = makeSut()
+    const isValidSpy = jest.spyOn(addQuestionStub, 'add')
+    const httpRequest = {
+      body: {
+        question: 'valid_question'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('valid_question')
   })
 })
