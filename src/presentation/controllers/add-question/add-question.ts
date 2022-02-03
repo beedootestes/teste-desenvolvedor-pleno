c18af7244a95a1853635178e4bf12d0b34172463
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-03 08:56:30 -0300

    feat: ensure addQuestion Controller throws when addQuestions throws

--- src/presentation/controllers/add-question/add-question.ts
+++ src/presentation/controllers/add-question/add-question.ts
@@ -5,16 +5,22 @@ import { badRequest, ok } from '../../helpers/http-helpers'
 export class AddQuestionController implements Controller {
   private readonly addQuestion: AddQuestion
 
-  constructor (addQuestion) {
+  constructor(addQuestion) {
     this.addQuestion = addQuestion
   }
 
-  handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.question) {
-      return badRequest(new MissingParamError('question'))
+  handle(httpRequest: HttpRequest): HttpResponse {
+    try {
+      if (!httpRequest.body.question) {
+        return badRequest(new MissingParamError('question'))
+      }
+      const { question } = httpRequest.body
+      this.addQuestion.add(question)
+      return ok()
+    } catch (error) {
+      return {
+        statusCode: 500, body: 'Server Errror'
+      }
     }
-    const { question } = httpRequest.body
-    this.addQuestion.add(question)
-    return ok()
   }
 }
