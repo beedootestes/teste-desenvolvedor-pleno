a6874b77e779c796af2ad4ca02a3fd85501b8ba7
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-03 15:00:51 -0300

    chore: mongodb config

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,18 @@
+import { MongoClient, Collection } from 'mongodb'
+
+export const MongoHelper = {
+  client: MongoClient,
+
+  async connect (uri: string): Promise<void> {
+    this.client = await MongoClient.connect(process.env.MONGO_URL as string, {
+    })
+  },
+
+  async disconnect (): Promise<void> {
+    await this.client.close()
+  },
+
+  getCollection (name: string): Collection {
+    return this.client.db().collection(name)
+  }
+}
