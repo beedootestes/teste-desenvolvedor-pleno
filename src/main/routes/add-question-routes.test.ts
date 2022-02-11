5e95daf7f6c66800209b5a992d1a8e5802e7406c
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-11 16:46:30 -0300

    refactor: connect to mongodb before integrations tests

--- src/main/routes/add-question-routes.test.ts
+++ src/main/routes/add-question-routes.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 import app from '../config/app'
 
 describe('Add Question', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const questionsCollection = MongoHelper.getCollection('questions')
+    await questionsCollection.deleteMany({})
+  })
+
   test('Should return question success', async () => {
     await request(app)
       .post('/api/add-question')
