556a0c4cf8078b1c4afec416cf3c556fb3389105
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-15 16:25:09 -0300

    feat: ensure LogControllerDecoreator calls controller with correct value

--- src/main/factories/add-question.ts
+++ src/main/factories/add-question.ts
@@ -1,9 +1,12 @@
 import { AddQuestionController } from '../../presentation/controllers/add-question/add-question'
 import { DbAddQuestion } from '../../data/usecases/add-question/db-add-question'
 import { QuestionMongoRepository } from '../../infra/db/mongodb/question-repositoy/question'
+import { LogControllerDecorator } from '../decorators/log'
+import { Controller } from '../../presentation/protocols'
 
-export const makeAddQuestionController = (): AddQuestionController => {
+export const makeAddQuestionController = (): Controller => {
   const questionMongoRepository = new QuestionMongoRepository()
   const dbAddQuestion = new DbAddQuestion(questionMongoRepository)
-  return new AddQuestionController(dbAddQuestion)
+  const addQuestionController = new AddQuestionController(dbAddQuestion)
+  return new LogControllerDecorator(addQuestionController)
 }
