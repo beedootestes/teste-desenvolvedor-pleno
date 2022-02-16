8855fa91050a5f4c97cd7e5a02f4da48385e685e
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-15 21:48:53 -0300

    refactor: remove duplicated code

--- src/data/usecases/add-question/db-add-question.spec.ts
+++ src/data/usecases/add-question/db-add-question.spec.ts
@@ -4,6 +4,15 @@ import { AddQuestionRepository } from '../../protocols/add-account-repository'
 import { DbAddQuestion } from './db-add-question'
 
 describe('DBAddQuestion', () => {
+  const makeFakeQuestion = (): AddQuestionModel => ({
+    question: 'valid_question'
+  })
+
+  const makeFakeResponse = (): QuestionModel => ({
+    id: 'any_id',
+    question: 'valid_question'
+  })
+
   interface Sut {
     sut: DbAddQuestion
     addQuestionRepositoryStub: AddQuestionRepository
@@ -20,10 +29,7 @@ describe('DBAddQuestion', () => {
   const makeAddQuestionRepositoryStub = (): AddQuestionRepository => {
     class AddQuestionRepositoryStub implements AddQuestionRepository {
       async add (question: AddQuestionModel): Promise<QuestionModel> {
-        const fakeQuestion = {
-          id: 'valid_id',
-          question: 'valid_question'
-        }
+        const fakeQuestion = makeFakeResponse()
         return await new Promise(resolve => resolve(fakeQuestion))
       }
     }
@@ -33,22 +39,15 @@ describe('DBAddQuestion', () => {
   test('Should call addQuestionRepository', async () => {
     const { sut, addQuestionRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(addQuestionRepositoryStub, 'add')
-    const question = {
-      question: 'valid_question'
-    }
+    const question = makeFakeQuestion()
     await sut.add(question)
     expect(addSpy).toHaveBeenLastCalledWith(question)
   })
 
   test('Should return a question on success', async () => {
     const { sut } = makeSut()
-    const question = {
-      question: 'valid_question'
-    }
+    const question = makeFakeQuestion()
     const result = await sut.add(question)
-    expect(result).toEqual({
-      id: 'valid_id',
-      question: 'valid_question'
-    })
+    expect(result).toEqual(makeFakeResponse())
   })
 })
--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -3,6 +3,25 @@ import { serverError } from '../../presentation/helpers/http-helpers'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
 import { LogControllerDecorator } from './log'
 
+const makeFakeServerError = (): HttpResponse => {
+  const fakeError = new Error()
+  fakeError.stack = 'any_stack'
+  return serverError(fakeError)
+}
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    question: 'valid_question'
+  }
+})
+
+const makeFakeResponse = (): HttpResponse => ({
+  body: {
+    question: 'valid_question'
+  },
+  statusCode: 200
+})
+
 interface SutTypes {
   sut: LogControllerDecorator
   controllerStub: Controller
@@ -12,12 +31,7 @@ interface SutTypes {
 const makeSut = (): SutTypes => {
   class ControllerStub implements Controller {
     async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-      const httpResponse: HttpResponse = {
-        body: {
-          question: 'valid_question'
-        },
-        statusCode: 200
-      }
+      const httpResponse: HttpResponse = makeFakeResponse()
       return await new Promise(resolve => resolve(httpResponse))
     }
   }
@@ -44,43 +58,23 @@ describe('LogController Decorator', () => {
   test('Should call controller with correct values', async () => {
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const httpRequest = {
-      body: {
-        question: 'valid_question'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
   })
 
   test('Should return the same result of controller', async () => {
     const { sut } = makeSut()
-    const httpRequest = {
-      body: {
-        question: 'valid_question'
-      }
-    }
+    const httpRequest = makeFakeRequest()
     const httpResponse = await sut.handle(httpRequest)
-    expect(httpResponse).toEqual({
-      body: {
-        question: 'valid_question'
-      },
-      statusCode: 200
-    })
+    expect(httpResponse).toEqual(makeFakeResponse())
   })
 
   test('Should call logErrorRepository with correct error if controller returns a server error', async () => {
     const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
-    const fakeError = new Error()
-    fakeError.stack = 'any_stack'
-    const error = serverError(fakeError)
     const logSpy = jest.spyOn(logErrorRepositoryStub, 'log')
-    jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => resolve(error)))
-    const httpRequest = {
-      body: {
-        question: 'valid_question'
-      }
-    }
+    jest.spyOn(controllerStub, 'handle').mockReturnValueOnce(new Promise(resolve => resolve(makeFakeServerError())))
+    const httpRequest = makeFakeRequest()
     await sut.handle(httpRequest)
     expect(logSpy).toHaveBeenCalledWith('any_stack')
   })
