3fb8dd47457675175fff840ade8eccf5255e9f3a
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-17 14:31:01 -0300

    feat: add MongoRepository to questionFactory

--- src/main/factories/add-question.ts
+++ src/main/factories/add-question.ts
@@ -3,10 +3,12 @@ import { DbAddQuestion } from '../../data/usecases/add-question/db-add-question'
 import { QuestionMongoRepository } from '../../infra/db/mongodb/question-repositoy/question'
 import { LogControllerDecorator } from '../decorators/log'
 import { Controller } from '../../presentation/protocols'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 
 export const makeAddQuestionController = (): Controller => {
   const questionMongoRepository = new QuestionMongoRepository()
   const dbAddQuestion = new DbAddQuestion(questionMongoRepository)
   const addQuestionController = new AddQuestionController(dbAddQuestion)
-  return new LogControllerDecorator(addQuestionController)
+  const logMongoRepository = new LogMongoRepository()
+  return new LogControllerDecorator(addQuestionController, logMongoRepository)
 }
