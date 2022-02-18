35e81fc357583450a14ec1357ce4c780a41dfd8d
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-18 16:52:03 -0300

    feat: ensure QuestionMongoRepository list return a list of questions

--- src/infra/db/mongodb/question-repositoy/question.ts
+++ src/infra/db/mongodb/question-repositoy/question.ts
@@ -1,11 +1,22 @@
 import { AddQuestionRepository } from '../../../../data/protocols/add-question-repository'
+import { ListQuestionsRepository } from '../../../../data/protocols/list-question-repository'
 import { AddQuestionModel, QuestionModel, MongoHelper } from './question-protococols'
 
-export class QuestionMongoRepository implements AddQuestionRepository {
+export class QuestionMongoRepository implements AddQuestionRepository, ListQuestionsRepository {
   async add (questionData: AddQuestionModel): Promise<QuestionModel> {
     const questionCollection = await MongoHelper.getCollection('questions')
     const result = await questionCollection.insertOne(questionData)
     const id = result.insertedId.toString()
     return Object.assign({}, { id: id }, { question: questionData.question })
+  }
+
+  async list (): Promise<QuestionModel[]> {
+    const questionCollection = await MongoHelper.getCollection('questions')
+    const result = await questionCollection.find().toArray()
+    const questions = result.map(question => ({
+      id: question._id.toString(),
+      question: question.question
+    }))
+    return questions
   }
 }
