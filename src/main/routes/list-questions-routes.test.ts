4ccdd33adc5f93c48d06ab4da95e6d15ec8bb710
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-18 17:40:39 -0300

    test: ensure routes api/list-questions integrates correctly

---
+++ src/main/routes/list-questions-routes.test.ts
@@ -0,0 +1,25 @@
+import request from 'supertest'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import app from '../config/app'
+
+describe('List Questions', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const questionsCollection = await MongoHelper.getCollection('questions')
+    await questionsCollection.deleteMany({})
+  })
+
+  test('Should return question success', async () => {
+    await request(app)
+      .get('/api/list-questions')
+      .send()
+      .expect(200)
+  })
+})
