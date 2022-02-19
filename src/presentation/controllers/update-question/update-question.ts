b2029709154af2af2644dffb055b2f12039f1d56
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-19 15:55:27 -0300

    feat: ensure UdateQuestionController return 500 if updateQuestion throws

--- src/presentation/controllers/update-question/update-question.ts
+++ src/presentation/controllers/update-question/update-question.ts
@@ -1,5 +1,5 @@
 import { UpdateQuestion, Controller, HttpRequest, HttpResponse, Validation } from './update-question-protocols'
-import { ok } from '../../helpers/http-helpers'
+import { ok, serverError } from '../../helpers/http-helpers'
 
 export class UpdateQuestionController implements Controller {
   private readonly updateQuestion: UpdateQuestion
@@ -11,8 +11,12 @@ export class UpdateQuestionController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const question = { ...httpRequest.body, ...httpRequest.params }
-    const result = await this.updateQuestion.update(question)
-    return ok(result)
+    try {
+      const question = { ...httpRequest.body, ...httpRequest.params }
+      const result = await this.updateQuestion.update(question)
+      return ok(result)
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
