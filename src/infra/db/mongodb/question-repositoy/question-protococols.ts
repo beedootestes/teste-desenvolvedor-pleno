201edc078ef18cce525feca8c400b319744a5687
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-19 17:31:38 -0300

    feat: ensure QuestionMongoRepository returns a question when it updates

--- src/infra/db/mongodb/question-repositoy/question-protococols.ts
+++ src/infra/db/mongodb/question-repositoy/question-protococols.ts
@@ -1,3 +1,4 @@
 export * from '../../../../domain/models/question'
 export * from '../../../../domain/usecases/add-question'
+export * from '../../../../domain/usecases/update-question'
 export * from '../helpers/mongo-helper'
--- src/infra/db/mongodb/question-repositoy/question.ts
+++ src/infra/db/mongodb/question-repositoy/question.ts
@@ -1,16 +1,20 @@
 import { AddQuestionRepository } from '../../../../data/protocols/add-question-repository'
 import { ListQuestionsRepository } from '../../../../data/protocols/list-question-repository'
-import { AddQuestionModel, QuestionModel, MongoHelper } from './question-protococols'
+import { UpdateQuestionRepository } from '../../../../data/protocols/update-question-repository'
+import { AddQuestionModel, QuestionModel, MongoHelper, UpdateQuestionModel } from './question-protococols'
 
-export class QuestionMongoRepository implements AddQuestionRepository, ListQuestionsRepository {
-  async add (questionData: AddQuestionModel): Promise<QuestionModel> {
+export class QuestionMongoRepository implements
+  AddQuestionRepository,
+  ListQuestionsRepository,
+  UpdateQuestionRepository {
+  async add(questionData: AddQuestionModel): Promise<QuestionModel> {
     const questionCollection = await MongoHelper.getCollection('questions')
     const result = await questionCollection.insertOne(questionData)
     const id = result.insertedId.toString()
     return Object.assign({}, { id: id }, { question: questionData.question })
   }
 
-  async list (): Promise<QuestionModel[]> {
+  async list(): Promise<QuestionModel[]> {
     const questionCollection = await MongoHelper.getCollection('questions')
     const result = await questionCollection.find().toArray()
     const questions = result.map(question => ({
@@ -18,5 +22,23 @@ export class QuestionMongoRepository implements AddQuestionRepository, ListQuest
       question: question.question
     }))
     return questions
+  }
+
+  async update(questionData: UpdateQuestionModel): Promise<QuestionModel> {
+    const questionCollection = await MongoHelper.getCollection('questions')
+    const result = await questionCollection.updateOne(
+      {
+        _id: questionData.id
+      }, {
+        $set: {
+          question: questionData.question
+        }
+      },
+      {
+        upsert: true
+      }
+    )
+    const id = result.upsertedId.toString()
+    return Object.assign({}, { id: id }, { question: questionData.question })
   }
 }
