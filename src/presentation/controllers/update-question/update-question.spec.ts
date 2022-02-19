580cc0c6ce6e8430e12aa003604c3e6421e78c17
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-19 16:20:50 -0300

    test: ensure UpdateQuestionController returns 400 if validates throws

--- src/presentation/controllers/update-question/update-question.spec.ts
+++ src/presentation/controllers/update-question/update-question.spec.ts
@@ -1,4 +1,5 @@
-import { serverError } from '../../helpers/http-helpers'
+import { MissingParamError } from '../../errors/missing-param-error'
+import { badRequest, serverError } from '../../helpers/http-helpers'
 import { UpdateQuestionController } from './update-question'
 import { HttpRequest, QuestionModel, UpdateQuestion, UpdateQuestionModel, Validation } from './update-question-protocols'
 
@@ -86,5 +87,22 @@ describe('Update Question Controller', () => {
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
     expect(validateSpy).toHaveBeenLastCalledWith(httpRequest.params)
     expect(validateSpy).toHaveBeenCalledTimes(2)
+  })
+
+  test('Should returns 400 if validators throws', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
+  })
+
+  test('Should returns 400 if validators throws at param', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(null)
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new MissingParamError('any_field'))
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('any_field')))
   })
 })
