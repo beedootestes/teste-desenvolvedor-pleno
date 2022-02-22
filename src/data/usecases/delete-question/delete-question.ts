a68152e1ef7c3d4e6f71ca7a8d97eb18103e2162
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-22 16:59:44 -0300

    feat: ensure DbDeleteQuestion calls getQuestionRepository with correct values

--- src/data/usecases/delete-question/delete-question.ts
+++ src/data/usecases/delete-question/delete-question.ts
@@ -1,14 +1,18 @@
 import { DeleteQuestion } from '../../../domain/usecases/delete-question'
 import { DeleteQuestionRepository } from '../../protocols/delete-question-repository'
+import { GetQuestionRepository } from '../../protocols/get-question-repository'
 
 export class DbDeleteQuestion implements DeleteQuestion {
   private readonly deleteQuestionRepository: DeleteQuestionRepository
+  private readonly getQuestionRepository: GetQuestionRepository
 
-  constructor (deleteQuestionRepository: DeleteQuestionRepository) {
+  constructor (deleteQuestionRepository: DeleteQuestionRepository, getQuestionRepository: GetQuestionRepository) {
     this.deleteQuestionRepository = deleteQuestionRepository
+    this.getQuestionRepository = getQuestionRepository
   }
 
   async delete (id: string): Promise<Boolean> {
+    await this.getQuestionRepository.get(id)
     await this.deleteQuestionRepository.delete(id)
     return true
   }
