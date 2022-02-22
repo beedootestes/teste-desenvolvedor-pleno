2637af44da5df4949e089609cdd9115fc1aa0d03
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-22 16:03:51 -0300

    feat: ensure DeleteQuestionController calls validation with correct value

--- src/presentation/controllers/delete-question/delete-question.ts
+++ src/presentation/controllers/delete-question/delete-question.ts
@@ -1,15 +1,19 @@
 import { DeleteQuestion } from '../../../domain/usecases/delete-question'
-import { Controller, HttpRequest, HttpResponse } from './delete-question-protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from './delete-question-protocols'
 
 export class DeleteQuestionController implements Controller {
   private readonly deleteQuestion: DeleteQuestion
+  private readonly validation: Validation
 
-  constructor (deleteQuestion) {
+  constructor (deleteQuestion: DeleteQuestion, validation: Validation) {
     this.deleteQuestion = deleteQuestion
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const id: string = httpRequest.params.id
+    this.validation.validate(httpRequest.params)
+
     await this.deleteQuestion.delete(id)
     return { body: true, statusCode: 200 }
   }
