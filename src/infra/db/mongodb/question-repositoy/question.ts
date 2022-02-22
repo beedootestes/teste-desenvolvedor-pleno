8792d429bd54d947e5c6a05b91bfddb167bfce9b
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-22 17:50:23 -0300

    feat: ensure QuestionMongoRepository should return true when deletion was a success

--- src/infra/db/mongodb/question-repositoy/question.ts
+++ src/infra/db/mongodb/question-repositoy/question.ts
@@ -1,5 +1,6 @@
 import { ObjectId } from 'mongodb'
 import { AddQuestionRepository } from '../../../../data/protocols/add-question-repository'
+import { DeleteQuestionRepository } from '../../../../data/protocols/delete-question-repository'
 import { GetQuestionRepository } from '../../../../data/protocols/get-question-repository'
 import { ListQuestionsRepository } from '../../../../data/protocols/list-question-repository'
 import { UpdateQuestionRepository } from '../../../../data/protocols/update-question-repository'
@@ -9,7 +10,8 @@ export class QuestionMongoRepository implements
   AddQuestionRepository,
   ListQuestionsRepository,
   UpdateQuestionRepository,
-  GetQuestionRepository {
+  GetQuestionRepository,
+  DeleteQuestionRepository {
   async add (questionData: AddQuestionModel): Promise<QuestionModel> {
     const questionCollection = await MongoHelper.getCollection('questions')
     const result = await questionCollection.insertOne(questionData)
@@ -52,5 +54,12 @@ export class QuestionMongoRepository implements
       ? Object.assign({}, { id: id }, { question: question.question })
       : question
     return result
+  }
+
+  async delete (id: string): Promise<Boolean> {
+    const questionCollection = await MongoHelper.getCollection('questions')
+    const result = await questionCollection.deleteOne({ _id: new ObjectId(id) })
+    console.log(result)
+    return result.deletedCount === 1 ? result.acknowledged : false
   }
 }
