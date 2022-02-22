abae9540fbcb0893d3f0ca9da7a903df8922e81d
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-22 13:35:18 -0300

    test: ensure QuestionMongorePository .get returns correct values

--- src/infra/db/mongodb/question-repositoy/question.spec.ts
+++ src/infra/db/mongodb/question-repositoy/question.spec.ts
@@ -35,7 +35,6 @@ describe('Question Mongo repository', () => {
       question: 'any_question'
     })
     const questions = await sut.list()
-    console.log(questions)
     expect(questions).toBeTruthy()
     expect(questions[0].id).toBeTruthy()
     expect(questions[0].question).toBe('any_question')
@@ -51,5 +50,16 @@ describe('Question Mongo repository', () => {
     expect(question.id).toBeTruthy()
     expect(question.question).toBe('any_question')
     expect(question.id).toBe('any_id')
+  })
+
+  test('Should return a question when get is a success', async () => {
+    const questionsCollection = await MongoHelper.getCollection('questions')
+    await questionsCollection.insertOne({ question: 'Fake question' })
+    const fakeQuestion = await questionsCollection.findOne({ question: 'Fake question' })
+
+    const id = fakeQuestion?._id.toString().valueOf() ?? 'any_id'
+    const sut = makeSut()
+    const question = await sut.get(id)
+    expect(question).toBeTruthy()
   })
 })
