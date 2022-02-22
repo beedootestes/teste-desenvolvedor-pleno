35994142a8a799b40159bfe761444cc03986597d
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-22 19:54:14 -0300

    test: ensure AddResponseController calls addResponse with correct values

---
+++ src/presentation/controllers/add-responses/add-response.spec.ts
@@ -0,0 +1,54 @@
+import { AddResponseController } from './add-response'
+import { AddResponse, AddResponseModel, HttpRequest, ResponseModel } from './add-response-protocols'
+
+describe('AddResponseController', () => {
+  const makeFakeResponse = (): ResponseModel => ({
+    id: 'valid_response_id',
+    response: 'valid_response',
+    questions: ['valid_question_id']
+  })
+
+  const makeFakeRequest = (): HttpRequest => ({
+    body: {
+      response: 'valid_response'
+    },
+    params: {
+      id: 'valid_question_id'
+    }
+  })
+
+  interface SutType {
+    sut: AddResponseController
+    addResponseStub: AddResponse
+  }
+
+  const makeSut = (): SutType => {
+    const addResponseStub = makeAddResponse()
+    const sut = new AddResponseController(addResponseStub)
+    return {
+      sut,
+      addResponseStub
+    }
+  }
+
+  const makeAddResponse = (): AddResponse => {
+    class AddResponseStub implements AddResponse {
+      async add (response: AddResponseModel): Promise<ResponseModel> {
+        const fakeResponse = makeFakeResponse()
+        return await new Promise(resolve => resolve(fakeResponse))
+      }
+    }
+    return new AddResponseStub()
+  }
+
+  test('Should call addResponse with correct values', async () => {
+    const { sut, addResponseStub } = makeSut()
+    const isValidSpy = jest.spyOn(addResponseStub, 'add')
+    const httpRequest = makeFakeRequest()
+    await sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith({
+      response: 'valid_response',
+      id: 'valid_question_id'
+    })
+  })
+})
