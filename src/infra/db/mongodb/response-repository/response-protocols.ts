f1c9c444ea11dfba64eea30f3d5528b33799d40b
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-23 10:00:28 -0300

    feat: ensure ResponseMongoRepository add a response and returns it correctly

---
+++ src/infra/db/mongodb/response-repository/response-protocols.ts
@@ -0,0 +1,4 @@
+export * from '../helpers/mongo-helper'
+export * from '../../../../data/protocols/add-response-repository'
+export * from '../../../../domain/models/response'
+export * from '../../../../domain/usecases/add-response'
---
+++ src/infra/db/mongodb/response-repository/response.ts
@@ -0,0 +1,14 @@
+import { AddResponseModel, AddResponseRepository, MongoHelper, ResponseModel } from './response-protocols'
+
+export class ResponseMongoRepository implements AddResponseRepository {
+  async add (responseData: AddResponseModel): Promise<ResponseModel> {
+    const responseCollection = await MongoHelper.getCollection('responses')
+    const response = {
+      response: responseData.response,
+      questions: [responseData.id]
+    }
+    const result = await responseCollection.insertOne(response)
+    const id = result.insertedId.toString()
+    return Object.assign({}, { id: id }, { response: response.response, questions: response.questions })
+  }
+}
