cc5b72164f6d44da46911096d598e1c4162f2e07
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-23 10:00:44 -0300

    test: ensure ResponseMongoRepository add a response and returns it correctly

---
+++ src/infra/db/mongodb/response-repository/response.spec.ts
@@ -0,0 +1,33 @@
+import { ResponseMongoRepository } from './response'
+import { MongoHelper } from './response-protocols'
+
+describe('Response Mongo repository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const ResponsesCollection = await MongoHelper.getCollection('responses')
+    await ResponsesCollection.deleteMany({})
+  })
+
+  const makeSut = (): ResponseMongoRepository => {
+    return new ResponseMongoRepository()
+  }
+
+  test('Should return a Response on success', async () => {
+    const sut = makeSut()
+    const response = await sut.add({
+      response: 'any_response',
+      id: 'valid_question_id'
+    })
+    expect(response).toBeTruthy()
+    expect(response.id).toBeTruthy()
+    expect(response.response).toBe('any_response')
+    expect(response.questions[0]).toBe('valid_question_id')
+  })
+})
