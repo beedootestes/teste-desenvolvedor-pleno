8394f71c94e993fec29d3acd445b5fc799bd9c89
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-23 10:43:01 -0300

    feat: ensure DbAddResponse calls getQuestionRepository with correct values

--- src/data/usecases/add-response/add-response.ts
+++ src/data/usecases/add-response/add-response.ts
@@ -1,15 +1,19 @@
 import { ResponseModel } from '../../../domain/models/response'
 import { AddResponse, AddResponseModel } from '../../../domain/usecases/add-response'
 import { AddResponseRepository } from '../../protocols/add-response-repository'
+import { GetQuestionRepository } from '../../protocols/get-question-repository'
 
 export class DbAddResponse implements AddResponse {
   private readonly addResponseRepository: AddResponseRepository
+  private readonly getQuestionRepository: GetQuestionRepository
 
-  constructor (addResponseRepository: AddResponseRepository) {
+  constructor (addResponseRepository: AddResponseRepository, getQuestionRepository: GetQuestionRepository) {
     this.addResponseRepository = addResponseRepository
+    this.getQuestionRepository = getQuestionRepository
   }
 
-  async add (Response: AddResponseModel): Promise<ResponseModel> {
-    return await this.addResponseRepository.add(Response)
+  async add (response: AddResponseModel): Promise<ResponseModel> {
+    await this.getQuestionRepository.get(response.id)
+    return await this.addResponseRepository.add(response)
   }
 }
