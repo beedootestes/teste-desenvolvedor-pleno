f88bf2be3ab6653f222d7f73898f33a4412a8b2a
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-24 18:20:29 -0300

    feat: ensure listResponse calls getQuestionRepository with correct values

--- src/data/usecases/list-responses/list-responses.ts
+++ src/data/usecases/list-responses/list-responses.ts
@@ -1,14 +1,24 @@
 import { ListResponses } from '../../../domain/usecases/list-responses'
+import { InvalidParamError } from '../../../presentation/errors/invalid-param-error'
+import { GetQuestionRepository } from '../../protocols/get-question-repository'
 import { ListResponsesRepository } from '../../protocols/list-responses-repository'
 
 export class DbListResponses implements ListResponses {
   private readonly listResponsesRepository: ListResponsesRepository
+  private readonly getQuestionRepository: GetQuestionRepository
 
-  constructor (listResponsesRepository: ListResponsesRepository) {
+  constructor (
+    listResponsesRepository: ListResponsesRepository,
+    getQuestionRepository: GetQuestionRepository) {
+    this.getQuestionRepository = getQuestionRepository
     this.listResponsesRepository = listResponsesRepository
   }
 
   async list (id: string): Promise<string[]> {
+    const questionExist = await this.getQuestionRepository.get(id)
+    if (!questionExist) {
+      throw new InvalidParamError('id')
+    }
     return await this.listResponsesRepository.list(id)
   }
 }
