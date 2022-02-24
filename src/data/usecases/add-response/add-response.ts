3014b4000b1bc376f43794c63796a4d7dc3d013f
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-24 11:28:14 -0300

    feat: ensure DbAddResponse calls addResponseToQuestion with correct values

--- src/data/usecases/add-response/add-response.ts
+++ src/data/usecases/add-response/add-response.ts
@@ -2,15 +2,22 @@ import { ResponseModel } from '../../../domain/models/response'
 import { AddResponse, AddResponseModel } from '../../../domain/usecases/add-response'
 import { InvalidParamError } from '../../../presentation/errors/invalid-param-error'
 import { AddResponseRepository } from '../../protocols/add-response-repository'
+import { AddResponseToQuestionRepository } from '../../protocols/add-response-to-question.repository'
 import { GetQuestionRepository } from '../../protocols/get-question-repository'
 
 export class DbAddResponse implements AddResponse {
   private readonly addResponseRepository: AddResponseRepository
+  private readonly addResponseToQuestionRepository: AddResponseToQuestionRepository
   private readonly getQuestionRepository: GetQuestionRepository
 
-  constructor (addResponseRepository: AddResponseRepository, getQuestionRepository: GetQuestionRepository) {
+  constructor (
+    addResponseRepository: AddResponseRepository,
+    getQuestionRepository: GetQuestionRepository,
+    addResponseToQuestionRepository: AddResponseToQuestionRepository
+  ) {
     this.addResponseRepository = addResponseRepository
     this.getQuestionRepository = getQuestionRepository
+    this.addResponseToQuestionRepository = addResponseToQuestionRepository
   }
 
   async add (response: AddResponseModel): Promise<ResponseModel> {
@@ -18,6 +25,7 @@ export class DbAddResponse implements AddResponse {
     if (!questionExist) {
       throw new InvalidParamError('question_id')
     }
+    await this.addResponseToQuestionRepository.addResponse(response)
     return await this.addResponseRepository.add(response)
   }
 }
