dc4f569bbe5423bd6a883546519554216da70bc6
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-24 18:20:43 -0300

    test: ensure listResponse calls getQuestionRepository with correct values

--- src/data/usecases/list-responses/list-responses.spec.ts
+++ src/data/usecases/list-responses/list-responses.spec.ts
@@ -1,20 +1,31 @@
+import { QuestionModel } from '../../../domain/models/question'
+import { InvalidParamError } from '../../../presentation/errors/invalid-param-error'
+import { GetQuestionRepository } from '../../protocols/get-question-repository'
 import { ListResponsesRepository } from '../../protocols/list-responses-repository'
 import { DbListResponses } from './list-responses'
 
 describe('DBAddQuestion', () => {
   const makeFakeResponsesList = (): string[] => (['response 1', 'response 2'])
 
+  const makeFakeResponse = (): QuestionModel => ({
+    id: 'any_id',
+    question: 'updated_question',
+    responses: []
+  })
   interface SutType {
     sut: DbListResponses
     listResponsesRepositoryStub: ListResponsesRepository
+    getQuestionRepositoryStub: GetQuestionRepository
   }
 
   const makeSut = (): SutType => {
+    const getQuestionRepositoryStub = makeGetQuestionRepositoryStub()
     const listResponsesRepositoryStub = makeListResponsesRepositoryStub()
-    const sut = new DbListResponses(listResponsesRepositoryStub)
+    const sut = new DbListResponses(listResponsesRepositoryStub, getQuestionRepositoryStub)
     return {
       sut,
-      listResponsesRepositoryStub
+      listResponsesRepositoryStub,
+      getQuestionRepositoryStub
     }
   }
 
@@ -28,6 +39,15 @@ describe('DBAddQuestion', () => {
     return new ListResponsesRepositoryStub()
   }
 
+  const makeGetQuestionRepositoryStub = (): GetQuestionRepository => {
+    class GetQuestionRepositoryStub implements GetQuestionRepository {
+      async get (id: string): Promise<any> {
+        return await new Promise(resolve => resolve(makeFakeResponse()))
+      }
+    }
+    return new GetQuestionRepositoryStub()
+  }
+
   test('Should call listResponsesRepository with no values', async () => {
     const { sut, listResponsesRepositoryStub } = makeSut()
     const addSpy = jest.spyOn(listResponsesRepositoryStub, 'list')
@@ -38,5 +58,25 @@ describe('DBAddQuestion', () => {
     const { sut } = makeSut()
     const result = await sut.list('valid_questio_id')
     expect(result).toEqual(makeFakeResponsesList())
+  })
+
+  test('Should call GetQuestionRepository correctly', async () => {
+    const { sut, getQuestionRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(getQuestionRepositoryStub, 'get')
+    await sut.list('valid_question_id')
+    expect(updateSpy).toBeCalledWith('valid_question_id')
+  })
+
+  test('Should throws if getQuestionRepository returns null', async () => {
+    const { sut, getQuestionRepositoryStub } = makeSut()
+    jest.spyOn(getQuestionRepositoryStub, 'get').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => resolve(null))
+    })
+
+    try {
+      await sut.list('any_id')
+    } catch (error) {
+      expect(error).toEqual(new InvalidParamError('id'))
+    }
   })
 })
