9bb8c52ae264adb34fb5f4b60e0a59095f99d9f2
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-24 16:08:47 -0300

    feat: remove AddResponseRepository implementations from AddResponse routes

--- src/domain/usecases/add-response.ts
+++ src/domain/usecases/add-response.ts
@@ -1,10 +1,8 @@
-import { ResponseModel } from '../models/response'
-
 export interface AddResponseModel{
   response: string
   question_id: string
 }
 
 export interface AddResponse {
-  add (question: AddResponseModel): Promise<ResponseModel>
+  add (question: AddResponseModel): Promise<Boolean>
 }
--- src/main/factories/add-response/add-response.ts
+++ src/main/factories/add-response/add-response.ts
@@ -1,16 +1,14 @@
 import { DbAddResponse } from '../../../data/usecases/add-response/add-response'
 import { LogMongoRepository } from '../../../infra/db/mongodb/log-repository/log'
 import { QuestionMongoRepository } from '../../../infra/db/mongodb/question-repositoy/question'
-import { ResponseMongoRepository } from '../../../infra/db/mongodb/response-repository/response'
 import { AddResponseController } from '../../../presentation/controllers/add-response/add-response'
 import { Controller } from '../../../presentation/protocols'
 import { LogControllerDecorator } from '../../decorators/log'
 import { makeAddResponseValidation } from './add-response-validation'
 
 export const makeAddResponseController = (): Controller => {
-  const responseMongoRepository = new ResponseMongoRepository()
   const questionMongoRepository = new QuestionMongoRepository()
-  const dbAddResponse = new DbAddResponse(responseMongoRepository, questionMongoRepository, questionMongoRepository)
+  const dbAddResponse = new DbAddResponse(questionMongoRepository, questionMongoRepository)
   const addResponseController = new AddResponseController(dbAddResponse, makeAddResponseValidation())
   const logMongoRepository = new LogMongoRepository()
   return new LogControllerDecorator(addResponseController, logMongoRepository)
