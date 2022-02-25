e4a4333e86e78d109d612983b7965716d82dae50
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-25 13:52:58 -0300

    test: ensure: DbUpdateResponse calls getQuestionRepository correctly

--- src/data/usecases/update-response/db-update-response.spec.ts
+++ src/data/usecases/update-response/db-update-response.spec.ts
@@ -1,4 +1,5 @@
 import { UpdateResponseModel } from '../../../domain/usecases/update-response'
+import { InvalidParamError } from '../../../presentation/errors/invalid-param-error'
 import { GetQuestionRepository } from '../../protocols/get-question-repository'
 import { UpdateResponseRepository } from '../../protocols/update-response-repository'
 import { DbUpdateResponse } from './db-update-response'
@@ -63,5 +64,26 @@ describe('DBupdateResponse', () => {
     const response = makeFakeResponse()
     const result = await sut.update(response)
     expect(result).toEqual(true)
+  })
+
+  test('Should call getQuestionRepository', async () => {
+    const { sut, getQuestionRepositoryStub } = makeSut()
+    const updateSpy = jest.spyOn(getQuestionRepositoryStub, 'get')
+    const response = makeFakeResponse()
+    await sut.update(response)
+    expect(updateSpy).toHaveBeenLastCalledWith(response.question_id)
+  })
+
+  test('Should throws if getQuestionRepository returns null', async () => {
+    const { sut, getQuestionRepositoryStub } = makeSut()
+    jest.spyOn(getQuestionRepositoryStub, 'get').mockImplementationOnce(async () => {
+      return await new Promise((resolve, reject) => resolve(null))
+    })
+    const response = makeFakeResponse()
+    try {
+      await sut.update(response)
+    } catch (error) {
+      expect(error).toEqual(new InvalidParamError('question_id'))
+    }
   })
 })
