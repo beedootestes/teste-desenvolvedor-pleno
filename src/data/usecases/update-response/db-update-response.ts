c1d09f2e89b3351dfa072fbdd8a191159a0abe89
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-25 13:48:50 -0300

    feat: ensure: DbUpdateResponse calls getQuestionRepository correctly

--- src/data/usecases/update-response/db-update-response.ts
+++ src/data/usecases/update-response/db-update-response.ts
@@ -1,4 +1,5 @@
 import { UpdateResponse, UpdateResponseModel } from '../../../domain/usecases/update-response'
+import { InvalidParamError } from '../../../presentation/errors/invalid-param-error'
 import { GetQuestionRepository } from '../../protocols/get-question-repository'
 import { UpdateResponseRepository } from '../../protocols/update-response-repository'
 
@@ -14,7 +15,11 @@ export class DbUpdateResponse implements UpdateResponse {
     this.getQuestionRepository = getQuestionRepository
   }
 
-  async update (Response: UpdateResponseModel): Promise<Boolean> {
-    return await this.updateResponseRepository.update(Response)
+  async update (response: UpdateResponseModel): Promise<Boolean> {
+    const questionExist = await this.getQuestionRepository.get(response.question_id)
+    if (!questionExist) {
+      throw new InvalidParamError('question_id')
+    }
+    return await this.updateResponseRepository.update(response)
   }
 }
