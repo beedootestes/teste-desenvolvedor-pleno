093bbf875258489c66dfd0411217ec1529bbdcd6
Fernando Silva <fernando.loures@mobitype.com.br>
2022-02-25 10:25:18 -0300

    feat: ensure updateResponseController calls validation with correct values

--- src/presentation/controllers/update-response/update-resonse.ts
+++ src/presentation/controllers/update-response/update-resonse.ts
@@ -1,16 +1,20 @@
 import { UpdateResponse } from '../../../domain/usecases/update-response'
 import { ok, serverError } from '../../helpers/http-helpers'
-import { Controller, HttpRequest, HttpResponse } from './update-response-protocols'
+import { Controller, HttpRequest, HttpResponse, Validation } from './update-response-protocols'
 
 export class UpdateResponseController implements Controller {
   private readonly updateResponse: UpdateResponse
+  private readonly validation: Validation
 
-  constructor (updateResponse) {
+  constructor (updateResponse, validation) {
     this.updateResponse = updateResponse
+    this.validation = validation
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
+      this.validation.validate({ ...httpRequest.params, ...httpRequest.body })
+
       const newResponse = {
         new_response: httpRequest.body.new_response,
         question_id: httpRequest.params.question_id
