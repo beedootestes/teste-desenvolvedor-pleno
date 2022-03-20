f1eeca8607c0f353e2ddb459b04e79d1c89423ba
jobserver <joabe.santos@gmail.com>
2022-03-20 11:01:07 -0300

    test: fix new question and list

--- src/modules/questions/routes/question-routes.test.js
+++ src/modules/questions/routes/question-routes.test.js
@@ -3,10 +3,15 @@ import app from '../../../config/http/app';
 
 describe ('Question Router', () => {
   test ('Should return an list of questions on sucess', async () => {
-    await request (app).get ('/api/question').expect (200);
+    
+    const req = await request (app).get ('/api/question');
+    
+    expect(req.status).toBe(200)       
+
   });
 
   test ('should fail if the question is null or length < 1', async () => {
+    
     const req = await request (app).post ('/api/question').send ({
       question: '',
     });
@@ -15,12 +20,12 @@ describe ('Question Router', () => {
   });
 
   test ('Should be able to enter a question', async () => {
+    
     const req = await request (app).post ('/api/question').send ({
       question: 'Qual país ?',
     });
 
-    expect (req.body).toEqual (
-      {message: 'Question registered successfully'});
+    expect (req.body.message).toBe ('Question registered successfully');
   });
 
 
