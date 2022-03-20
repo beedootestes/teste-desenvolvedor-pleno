07966584426eb0fa22270c6856345954472de523
jobserver <joabe.santos@gmail.com>
2022-03-20 11:04:30 -0300

    feat: add filtered by id

---
+++ src/modules/questions/controllers/get-by-question-controller.js
@@ -0,0 +1,20 @@
+import { Question } from '../models/question'
+
+export default class GetByQuestionController {
+  static async handle(req, res) {
+
+    const id  =  req.params.id
+        
+    try {
+      const questionData =  await Question.findById(id).select({__v:0})
+      res.status(200).json({
+        message:'question',
+        data: questionData
+      })
+    }
+    catch (error) {
+      res.status(500).json({message: error})
+    }
+
+  }
+}
--- src/modules/questions/routes/question-routes.js
+++ src/modules/questions/routes/question-routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express'
 
 import GetAllQuestionController from '../controllers/get-all-question-controller'
 import AddQuestionController from '../controllers/add-question-controller'
+import GetByQuestionController from '../controllers/get-by-question-controller'
 
 const router = Router()
 
 router.get('/', GetAllQuestionController.handle)
+
+router.get('/:id', GetByQuestionController.handle)
 
 router.post('/', AddQuestionController.handle)
 
