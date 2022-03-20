0d8c879be602ea8ff4a98e2420253ec8fe2430f2
jobserver <joabe.santos@gmail.com>
2022-03-20 02:08:52 -0300

    test: add new question

--- src/modules/questions/routes/question-routes.test.js
+++ src/modules/questions/routes/question-routes.test.js
@@ -1,10 +1,27 @@
-import request from 'supertest'
-import app from '../../../config/http/app'
+import request from 'supertest';
+import app from '../../../config/http/app';
 
-describe('Questions Routes', () => {
-  test('Should return an list of questions on sucess', async () => {
-    await request(app)
-      .get('/api/question')
-      .expect(200)
-  })
-})
+describe ('Question Router', () => {
+  test ('Should return an list of questions on sucess', async () => {
+    await request (app).get ('/api/question').expect (200);
+  });
+
+  test ('should fail if the question is null or length < 1', async () => {
+    const req = await request (app).post ('/api/question').send ({
+      question: '',
+    });
+
+    expect (req.status).toBe(422);
+  });
+
+  test ('Should be able to enter a question', async () => {
+    const req = await request (app).post ('/api/question').send ({
+      question: 'Qual país ?',
+    });
+
+    expect (req.body).toEqual (
+      {message: 'Question registered successfully'});
+  });
+
+
+});
