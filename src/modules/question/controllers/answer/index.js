af1ab7ba56d02554ee3f0ed8222df4ba666bf84f
jobserver <joabe.santos@gmail.com>
2022-03-22 01:25:10 -0300

    refactor: move imports in answer-routes to index controller answer

---
+++ src/modules/question/controllers/answer/index.js
@@ -0,0 +1,14 @@
+import AddAnswerController from './add-answer-controler'
+import UpdateAnswerController from './update-answer-controller'
+import DeleteAnswerController from './delete-answer-controller'
+import GetAnswerByQuestionController from './get-answer-by-question-controller'
+import GetAllAnswerQuestionController from './get-all-answer-question-controller'
+
+
+export {
+  AddAnswerController,
+  UpdateAnswerController,
+  DeleteAnswerController,
+  GetAnswerByQuestionController,
+  GetAllAnswerQuestionController,
+}
--- src/modules/question/routes/answer-routes.js
+++ src/modules/question/routes/answer-routes.js
@@ -1,9 +1,5 @@
 import { Router } from 'express'
-import AddAnswerController from '../controllers/answer/add-answer-controler'
-import UpdateAnswerController from '../controllers/answer/update-answer-controller'
-import DeleteQuestionController from '../controllers/answer/delete-answer-controller'
-import GetAnswerByQuestionController from '../controllers/answer/get-answer-by-question-controller'
-import GetAllAnswerQuestionController from '../controllers/answer/get-all-answer-question-controller'
+import { AddAnswerController, UpdateAnswerController,DeleteAnswerController, GetAllAnswerQuestionController, GetAnswerByQuestionController } from '../controllers/answer'
 
 
 const router = Router()
@@ -13,8 +9,8 @@ router.post('/',  AddAnswerController.handle)
 router.patch('/:id', UpdateAnswerController.handle)
 router.patch('/', UpdateAnswerController.handle)
 
-router.delete('/:id', DeleteQuestionController.handle)
-router.delete('/', DeleteQuestionController.handle)
+router.delete('/:id', DeleteAnswerController.handle)
+router.delete('/', DeleteAnswerController.handle)
 
 router.get('/question/', GetAllAnswerQuestionController.handle)
 router.get('/question/:questionId', GetAnswerByQuestionController.handle)
