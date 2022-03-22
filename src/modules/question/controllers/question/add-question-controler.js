7f6712695eb272108b098cae126947750947fceb
jobserver <joabe.santos@gmail.com>
2022-03-21 23:52:36 -0300

    refactor: http response and validation question

--- src/modules/question/controllers/question/add-question-controler.js
+++ src/modules/question/controllers/question/add-question-controler.js
@@ -1,3 +1,6 @@
+import { badRequest, createdResource } from '../../../../config/helpers/http-helper'
+import { isNotValidQuestion } from '../../helpers/question-helper'
+
 import { Question } from '../../models/question'
 
 export default class AddQuestionController {
@@ -6,9 +9,8 @@ export default class AddQuestionController {
 
     const {question} = req.body
 
-    if (!question || question.length === 0) {
-      res.status(422).json({message: 'A questão é obrigatória'})
-      return
+    if (isNotValidQuestion(question)) {
+      return badRequest(res,'A questão é obrigatória')      
     }
     
     const questionObj  = new Question({
@@ -17,17 +19,13 @@ export default class AddQuestionController {
 
     try {
       const newQuestion =  await questionObj.save()
-      
-      res.status(201).json(
-        {
-          message: 'Question registered successfully',
-          data:{
-            id: newQuestion.id
-          }
-        })
+
+      return createdResource(res,'Question registered successfully', {
+        id: newQuestion.id
+      })
     }
     catch (error) {      
-      res.status(500).json({message: error})
+      return serverError(res, error)
     }
 
   }
---
+++ src/modules/question/helpers/question-helper.js
@@ -0,0 +1,7 @@
+const isNotValidQuestion = (question) => {
+  return !question || question.length === 0
+}
+
+export {
+  isNotValidQuestion
+}
