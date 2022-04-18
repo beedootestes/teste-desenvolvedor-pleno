10789b3c969fcc06810f6a6cb8433b086a84f144
Mariana Garcia <maridgarcia045@gmail.com>
2022-04-18 17:16:39 -0300

    Inclui dados em seeder de Questions

---
+++ seeders/20220418200850-questions.js
@@ -0,0 +1,13 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, _Sequelize) => queryInterface.bulkInsert('Questions',
+    [
+      { question: "In what type of matter are atoms most tightly packed?" },
+      { question: "What is the nearest planet to the sun?" },
+      { question: "What is the hottest planet in the solar system?" },
+      { question: "What is the opposite of matter?" } 
+    ], {}),
+
+  down: async (queryInterface, Sequelize) => queryInterface.bulkDelete('Questions', null, {}),
+};
