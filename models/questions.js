0afd2e128de583c62acc415078d1b04f3833c39c
Mariana Garcia <maridgarcia045@gmail.com>
2022-04-18 17:03:23 -0300

    Cria model question.js referente a migration questions

--- models/questions.js
+++ models/questions.js
@@ -1,23 +1,10 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Questions extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Questions.init({
-    question: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Questions',
+const Question = (sequelize, DataTypes) => {
+  const Question = sequelize.define("Question", {
+    id: DataTypes.INTEGER,
+    question: DataTypes.STRING,
   });
-  return Questions;
+
+  return Question;
 };
+
+module.exports = Question;
