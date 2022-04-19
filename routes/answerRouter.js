68181a16fdba6a3816c9a140d9d13da5e088e978
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-19 18:58:57 -0300

    feat: Rota 'GET /answers'

--- routes/answerRouter.js
+++ routes/answerRouter.js
@@ -2,5 +2,6 @@ const answer = require('express').Router();
 const answerController = require('../controllers/AnswerController');
 
 answer.post('/', answerController.create);
+answer.get('/', answerController.getAll);
 
 module.exports = answer;
