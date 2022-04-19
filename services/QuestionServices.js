533b4ec87aa4ebbde25666618eff3cdc19c3a5aa
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-19 18:00:05 -0300

    feat: Implementa método getAll de question na camada services

--- services/QuestionServices.js
+++ services/QuestionServices.js
@@ -5,10 +5,20 @@ const create = async (question) => {
     const newQuestion = await Questions.create({ question });
     return newQuestion;
   } catch (error) {
-    console.error(`Error: Aqui ${error}`);
+    console.error(error);
   }
 }
 
+const getAll = async () => {
+  try {
+    const questions = await Questions.findAll();
+    return questions;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
 module.exports = {
   create,
+  getAll,
 };
