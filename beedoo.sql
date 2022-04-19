9c89397f582dfa35fd010f158c28bd640ae5c034
Mariana Garcia <maridgarcia045@gmail.com>
2022-04-19 20:36:19 -0300

    Corrige inserçao de dados no BD

--- beedoo.sql
+++ beedoo.sql
@@ -1,13 +1,19 @@
 DROP DATABASE IF EXISTS beedoo;
 CREATE DATABASE IF NOT EXISTS beedoo;
 
-CREATE TABLE beedoo.questions (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    question VARCHAR(255) DEFAULT 'new question',
-    answerOptions VARCHAR(255) DEFAULT 'option 1, option 2, option 3, option 4'
+CREATE TABLE beedoo.answers (
+  answerId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  answerOptions VARCHAR(255) DEFAULT 'option 1, option 2, option 3'
 );
 
-INSERT INTO beedoo.questions (question) VALUES 
-('question 1'),
-('question 2'),
-('question 3');
+INSERT INTO beedoo.answers (answerOptions) VALUES ('option 1, option 2, option 3');
+
+CREATE TABLE beedoo.questions (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    question VARCHAR(255) DEFAULT 'new question',
+    answerId INT NOT NULL,
+    FOREIGN KEY(answerId)
+    REFERENCES beedoo.answers(answerId)
+);
+
+INSERT INTO beedoo.questions (question, answerId) VALUES ('question 1', 1);
