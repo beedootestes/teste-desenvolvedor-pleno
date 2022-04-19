65d7d051505643cb067849bbe199b34ddc05f942
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-19 18:17:23 -0300

    feat: Rota 'PUT /questions/:id'

--- routes/QuestionsRouter.js
+++ routes/QuestionsRouter.js
@@ -2,6 +2,7 @@ const question = require('express').Router();
 const questionsController = require('../controllers/QuestionController');
 
 question.get('/:id', questionsController.getById);
+question.put('/:id', questionsController.update);
 question.post('/', questionsController.create);
 question.get('/', questionsController.getAll);
 
