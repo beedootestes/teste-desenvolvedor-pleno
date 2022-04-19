feffe1857e9291c5723cffbb11fd2a0247b6a6fd
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-19 18:00:50 -0300

    feat: Rota 'GET /questions'

--- routes/QuestionsRouter.js
+++ routes/QuestionsRouter.js
@@ -2,5 +2,6 @@ const question = require('express').Router();
 const questionsController = require('../controllers/QuestionController');
 
 question.post('/', questionsController.create);
+question.get('/', questionsController.getAll);
 
 module.exports = question;
