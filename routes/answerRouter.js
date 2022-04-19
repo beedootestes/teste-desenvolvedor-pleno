c28b85a57a95ae245d4399bb2bf03e6464ffcd28
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-19 19:55:48 -0300

    feat: Rota 'DELETE /answers/:id'

--- routes/answerRouter.js
+++ routes/answerRouter.js
@@ -2,6 +2,7 @@ const answer = require('express').Router();
 const answerController = require('../controllers/AnswerController');
 
 answer.put('/:id', answerController.update);
+answer.delete('/:id', answerController.deleteById);
 answer.post('/', answerController.create);
 answer.get('/', answerController.getAll);
 
