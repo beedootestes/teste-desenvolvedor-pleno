6be6334578158ef9b5835e2e5b279e2f15aa3a68
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-19 18:00:24 -0300

    feat: Implementa método getAll de question na camada controllers

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -2,11 +2,25 @@ const { StatusCodes } = require('http-status-codes');
 const questionsService = require('../services/QuestionServices');
 
 const create = async (req, res) => {
-  const { question } = req.body;
-  const newQuestion = await questionsService.create(question)
-  return res.status(StatusCodes.CREATED).json(newQuestion);
-}
+  try {
+    const { question } = req.body;
+    const newQuestion = await questionsService.create(question)
+    return res.status(StatusCodes.CREATED).json(newQuestion);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getAll = async (_req, res) => {
+  try {
+    const questions = await questionsService.getAll();
+    return res.status(StatusCodes.OK).json(questions);
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   create,
+  getAll,
 };
