0f86d2ca921ffffa4e4b4fd7ce6fc2644256b4c4
Ivanielson Cabral <ivanielson2011@gmail.com>
2022-04-20 10:10:30 -0300

    :test_tube: test: Implementa teste para rota 'GET /questions'

--- test/question.test.js
+++ test/question.test.js
@@ -49,3 +49,56 @@ describe("Rota 'POST /questions'", () => {
     });
   });
 });
+
+describe("Rota 'GET /questions'", () => {
+  const questions = [
+    {
+      id: 1,
+      question: 'Quem foi a primeira pessoa a viajar no Espaço?',
+    },
+    {
+      id: 2,
+      question: 'Qual a montanha mais alta do mundo?',
+    },
+    {
+      id: 3,
+      question: 'Que país tem o formato de uma bota?',
+    }
+  ];
+  before(async () => {
+    sinon
+      .stub(Questions, 'findAll')
+      .resolves(questions);
+  });
+
+  after(() => {
+    Questions.findAll.restore();
+  });
+
+  describe("Quando a requisição é feita com sucesso", () => {
+    let response;
+
+    before(async () => {
+      response = await chai
+        .request(app)
+        .get('/questions');
+    });
+
+    it("Deveria retorna http status 200", async () => {
+      expect(response).to.have.status(200);
+    });
+
+    it('Deveria retornar um objeto', () => {
+      expect(response.body).to.be.have.a('array');
+    });
+
+    it('Deveria retornar um array de objetos com as perguntas cadastradas', () => {
+      expect(response.body).to.be.length(3);
+    });
+
+    it("Deveria retornar um array de objetos com as propriedades 'id' e 'question'", () => {
+      expect(response.body[0]).to.be.have.a.property('id');
+      expect(response.body[0]).to.be.have.a.property('question');
+    });
+  });
+});
