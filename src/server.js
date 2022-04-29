43ed5eb57e7f4884e5ce0fe80fe83235a980ac0e
Douglas-Santana <douguinhodns@gmail.com>
2022-04-29 18:18:33 -0300

    :rocket: Deploy to heroku: Cria Procfile, altera npm start e implementa rota de boas vindas

--- src/server.js
+++ src/server.js
@@ -3,4 +3,10 @@ require('dotenv').config();
 
 const PORT = process.env.PORT;
 
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: "Welcome. Acess: https://github.com/douglas-santana/teste-desenvolvedor-pleno for how to use!"
+  })
+});
+
 app.listen(PORT, () => console.log(`servidor rodando na porta ${PORT}!`));
