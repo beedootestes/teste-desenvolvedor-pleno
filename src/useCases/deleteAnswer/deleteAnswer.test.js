27b2f264b9f7566042e6dd8d76e88aaa25783b71
Douglas-Santana <douguinhodns@gmail.com>
2022-04-29 13:53:08 -0300

    :heavy_check_mark: Conclui teste de integração de deletar uma asnwer por id

---
+++ src/useCases/deleteAnswer/deleteAnswer.test.js
@@ -0,0 +1,59 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiHttp = require('chai-http');
+const { Answers } = require("../../repositories/sequelize/models");
+const { app } = require('../../app');
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe("Teste de Integração rota DELETE/answers/:id", () => {
+
+  before(async () => {
+    sinon
+      .stub(Answers, 'create')
+      .resolves({ id: 1, answer: "É uma estrela." })
+      
+    sinon
+      .stub(Answers, 'destroy')
+      .resolves(null);
+  });
+
+  after(() => {
+    Answers.create.restore();
+  });
+    
+  describe("Deve ser capaz de deletar uma answer", async () => {
+
+    const answerData = {
+      answer: "É uma estrela.?"
+    };
+
+    let newAnswer;
+    let deleteAnswer;
+
+    before(async () => {
+      newAnswer = await chai
+        .request(app)
+        .post('/answers')
+        .send(answerData);
+    });
+
+    before(async () => {
+      deleteAnswer = await chai
+        .request(app)
+        .delete('/answers/1');
+    });
+
+    it("Deve retornar status 200", () => {
+      expect(deleteQuestion.status).to.be.equals(200);
+    });
+
+    it("Deve retornar a mensagem 'Answer Deleted!'", () => {
+      expect(deleteQuestion.body.message).to.be.equals('Answer Deleted!');
+    });
+
+  });
+
+});
